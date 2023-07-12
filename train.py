c7500ed02e1f9fd86eed00a5825df4ccd38ee1f6
charrywhite <35485042+charrywhite@users.noreply.github.com>
2023-07-12 12:21:11 +0800

    remove preprocessing JointTransform2D

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 import os
-
+import torch
 import torch.optim as optim
 
 from functools import partial
@@ -14,22 +14,22 @@ from unet.elastic_loss import EnergyLoss
 
 parser = ArgumentParser()
 parser.add_argument('--train_dataset', required=True, type=str)
-parser.add_argument('--val_dataset', type=str)
-parser.add_argument('--checkpoint_path', required=True, type=str)
-parser.add_argument('--device', default='cpu', type=str)
+parser.add_argument('--test_dataset', required=True,type=str)
+parser.add_argument('--save_path', required=True, type=str)
+parser.add_argument('--device', default='cuda', type=str)
 parser.add_argument('--in_channels', default=3, type=int)
 parser.add_argument('--out_channels', default=2, type=int)
-parser.add_argument('--depth', default=5, type=int)
+parser.add_argument('--depth', default=6, type=int)
 parser.add_argument('--alpha', default=0.35, type=float)
 parser.add_argument('--sigma', default=0.25, type=float)
-parser.add_argument('--width', default=32, type=int)
+parser.add_argument('--width', default=64, type=int)
 parser.add_argument('--epochs', default=100, type=int)
 parser.add_argument('--batch_size', default=1, type=int)
 parser.add_argument('--save_freq', default=0, type=int)
 parser.add_argument('--save_model', default=0, type=int)
 parser.add_argument('--model_name', type=str, default='model')
 parser.add_argument('--learning_rate', type=float, default=1e-3)
-parser.add_argument('--crop', type=int, default=None)
+parser.add_argument('--crop', type=int, default=512)
 args = parser.parse_args()
 
 if args.crop is not None:
@@ -37,19 +37,23 @@ if args.crop is not None:
 else:
     crop = None
 
-tf_train = JointTransform2D(crop=crop, p_flip=0.5, color_jitter_params=None, long_mask=True)
-tf_val = JointTransform2D(crop=crop, p_flip=0, color_jitter_params=None, long_mask=True)
-train_dataset = ImageToImage2D(args.train_dataset, tf_val)
-val_dataset = ImageToImage2D(args.val_dataset, tf_val)
-predict_dataset = Image2D(args.val_dataset)
+# tf_train = JointTransform2D(crop=crop)
+# tf_val = JointTransform2D(crop=crop)
+dataset =  ImageToImage2D(dataset_path = args.train_dataset)
+test_dataset = ImageToImage2D(dataset_path = args.test_dataset)
+train_size = int(0.8 * len(dataset))
+val_size = len(dataset) - train_size
+train_dataset, val_dataset = torch.utils.data.random_split(dataset, [train_size, val_size])
+print(f'Train dataset size is {len(train_dataset)},  val dataset size is {len(val_dataset)}, test dataset size is {len(test_dataset)}')
+
 
 conv_depths = [int(args.width*(2**k)) for k in range(args.depth)]
 unet = UNet2D(args.in_channels, args.out_channels, conv_depths)
-loss = EnergyLoss(cuda = True,alpha=args.alpha,sigma=args.sigma)
+loss = EnergyLoss(alpha=args.alpha) # use Elastic Loss here
 # loss = LogNLLLoss()
 optimizer = optim.Adam(unet.parameters(), lr=args.learning_rate)
 
-results_folder = os.path.join(args.checkpoint_path, args.model_name)
+results_folder = os.path.join(args.save_path, args.model_name)
 if not os.path.exists(results_folder):
     os.makedirs(results_folder)
 
@@ -60,5 +64,5 @@ model = Model(unet, loss, optimizer, results_folder, device=args.device)
 
 model.fit_dataset(train_dataset, n_epochs=args.epochs, n_batch=args.batch_size,
                   shuffle=True, val_dataset=val_dataset, save_freq=args.save_freq,
-                  save_model=args.save_model, predict_dataset=predict_dataset,
+                  save_model=args.save_model, predict_dataset=test_dataset,
                   metric_list=metric_list, verbose=True)
